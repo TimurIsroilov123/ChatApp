9b09388ec2e1b4c1179e68cb680f976bf5fbb8c4
Temur <temur.isroilov@akvelon.com>
2023-05-18 13:22:10 +0500

    Comment IOS specific code

--- shared/src/iosMain/kotlin/com/ti/chatapp/Platform.kt
+++ shared/src/iosMain/kotlin/com/ti/chatapp/Platform.kt
@@ -1,9 +1,9 @@
 package com.ti.chatapp
 
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
+//import platform.UIKit.UIDevice
+//
+//class IOSPlatform: Platform {
+//    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+//}
+//
+//actual fun getPlatform(): Platform = IOSPlatform()
